d3be8b77390b144df79721c478af69cdead8abf5
Alex McConnell <alexmmcconnell@gmail.com>
2021-12-08 23:31:57 -0700

    Test - Add tests for index and show pages

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,6 +1,49 @@
 require 'rails_helper'
 
 describe "Items API" do
+  it 'returns all items on index page' do
+    item_list = create_list(:item, 4)
+
+    get "/api/v1/items"
+
+    result_items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(result_items.count).to eq(4)
+
+    result_items.each do |item|
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
+
+  it 'returns all items on index page' do
+    item_list = create_list(:item, 4)
+    item = item_list.first
+
+    get "/api/v1/items/#{item.id}"
+
+    result_item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(result_item[:id]).to eq(item.id)
+    expect(result_item[:attributes][:name]).to eq(item.name)
+    expect(result_item[:attributes][:description]).to eq(item.description)
+    expect(result_item[:attributes][:unit_price]).to eq(item.unit_price)
+    expect(result_item[:attributes][:merchant_id]).to eq(item.merchant_id)
+  end
+
   it 'can find all items based on name' do
     item1 = create(:item, name: "Product")
     item2 = create(:item, name: "Duct Tape")
