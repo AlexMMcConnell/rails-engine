7cfa917372c2aef517f19adf2741da0100fa74f6
Alex McConnell <alexmmcconnell@gmail.com>
2021-12-09 00:36:39 -0700

    Feat - Add methods for CRUD process of items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,7 +7,42 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.show(params[:id])
   end
 
+  def create
+    item = Item.create(item_params)
+    if item.save
+      render json: ItemSerializer.show(item.id), status: 201
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item_params["merchant_id"].nil?
+      merchant_id = item.merchant_id
+    else
+      merchant_id = item_params["merchant_id"].to_i
+    end
+
+    if Merchant.find(merchant_id).present?
+      item.update(item_params)
+      render json: ItemSerializer.show(params[:id])
+    else
+      render status: 400
+    end
+  end
+
+  def destroy
+    item = ItemSerializer.show(params[:id])
+    Item.destroy(params[:id])
+    render json: item
+  end
+
   def find
     render json: ItemSerializer.find_all(params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
