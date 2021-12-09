16f54f6be58715c19dcb5b46db22ea5b1f20f0bb
Alex McConnell <alexmmcconnell@gmail.com>
2021-12-08 23:32:06 -0700

    Test - Adjust item factory so unit price is a float

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Name.name }
     description { Faker::Lorem.sentence }
-    unit_price { Faker::Number.number(digits: 2) } #needs to be a float
+    unit_price { Faker::Number.decimal(l_digits: 2, r_digits: 3).to_f } #needs to be a float
     merchant
   end
 end
