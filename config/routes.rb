f5e162202c0ae4d5688849af7d25f5e9afbdae10
Alex McConnell <alexmmcconnell@gmail.com>
2021-12-08 21:49:02 -0700

    Feat - Add routing for getting merchants and their items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants do
+        get "/api/v1/merchants/find"
+        get "/items", to: "merchant_items#index"
+      end
+      resources :items do
+
+      end
+    end
+  end
 end
